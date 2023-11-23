4986fa4699f1b453d9f11007ce006a4e7da01901
khuchuan <khuchuan@gmail.com>
2023-11-23 23:40:37 +0700

    update

---
+++ BlazorApp1/Pages/FetchData.razor.cs
@@ -0,0 +1,21 @@
+﻿using BlazorApp1.Data;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp1.Pages
+{
+    partial class FetchData
+    {
+        [Inject]
+        public WeatherForecastService ForecastService { get; set; }
+
+        private WeatherForecast[]? forecasts;
+
+        protected override async Task OnInitializedAsync()
+        {
+#if DEBUG
+            await Task.Delay(10000);
+#endif
+            forecasts = await ForecastService.GetForecastAsync(DateOnly.FromDateTime(DateTime.Now));
+        }
+    }
+}
