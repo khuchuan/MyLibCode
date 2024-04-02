76821115ddde951267c29f90593e906ad6d54ed4
khuchuan <khuchuan@gmail.com>
2024-04-03 05:54:03 +0700

    Add code

---
+++ BlazorAppTest/DTOs/StudentDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorAppTest.DTOs;
+
+public class StudentDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Address { get; set; }
+}
