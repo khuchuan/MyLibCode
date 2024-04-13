b24953519e23cd1b6bb9d24756c947ae1409e46f
khuchuan <khuchuan@gmail.com>
2024-04-13 21:24:52 +0700

    draff merchant form

---
+++ BlazorRealTest/Data/MerchantRepository.cs
@@ -0,0 +1,54 @@
+﻿using BlazorRealTest.Entity;
+
+namespace BlazorRealTest.Data;
+
+
+public class MerchantRepository : IMerchantRepository
+{
+    List<MerchantInfo> listMerchant = new List<MerchantInfo>();
+
+    public MerchantRepository()
+    {
+        for (int i = 0; i < 64; i++)
+        {
+            listMerchant.Add(new MerchantInfo
+            {
+                MerchantCode = $"MC{i}",
+                MerchantName = $"Merchant {i}",
+                Status = (short)new Random().Next(0, 2)
+            });
+        }
+    }
+
+    public List<MerchantInfo> GetMerchants()
+    {
+
+
+        return listMerchant;
+    }
+
+    public async Task<MerchantDTO> GetMerchantsAsync(short status, int page = 1, int pageSize = 10)
+    {
+        var list = listMerchant.FindAll(x => (x.Status <= 0 || x.Status == status));
+
+        var result = new MerchantDTO
+        {
+            Data = list.Skip((page - 1) * pageSize).Take(pageSize).ToList(),
+            PageSize = pageSize,
+            TotalCount = list.Count
+        };
+        return result;
+    }
+
+
+}
+
+public interface IMerchantRepository
+{
+    public List<MerchantInfo> GetMerchants();
+
+    public Task<MerchantDTO> GetMerchantsAsync(short status, int page, int pageSize);
+
+}
+
+
--- BlazorRealTest/Program.cs
+++ BlazorRealTest/Program.cs
@@ -1,4 +1,5 @@
 using BlazorRealTest.Components;
+using BlazorRealTest.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,8 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddAntDesign();
+
+builder.Services.AddTransient<IMerchantRepository, MerchantRepository>();
 
 var app = builder.Build();
 
