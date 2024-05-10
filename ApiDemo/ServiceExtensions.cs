745c1e39d4a3b2ecb82e6c39d4af0bc0a71914cf
khuchuan <khuchuan@gmail.com>
2024-05-10 20:35:09 +0700

    .

---
+++ ApiDemo/ServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure;
+using Infrastructure.Policies;
+
+namespace ApiDemo;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureHttpClientService(this IServiceCollection services)
+    {
+        services.AddHttpClient("").UseCircuitBreakerPolicy();
+    }
+
+ 
+}
---
+++ PollyConsole/ExecuteHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PollyConsole;
+
+internal sealed class ExecuteHelper
+{
+    public bool Fail { get; set; } = true;
+
+    public HttpResponseMessage ExecuteUnstable()
+    {
+        if (Fail)
+        {
+            Fail = false;
+            Console.WriteLine($"{DateTime.UtcNow}: Execute failed");
+            throw new InvalidOperationException();
+        }
+
+        Fail = true;
+        Console.WriteLine($"{DateTime.UtcNow}: Executed");
+        return new HttpResponseMessage(HttpStatusCode.OK);
+    }
+}
