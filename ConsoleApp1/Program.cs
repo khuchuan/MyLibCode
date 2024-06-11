f56e139522b3d13ddb8861e4c3314cbb40a8fe72
huankc <huankc@irismedia.vn>
2024-06-11 16:41:09 +0700

    .

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,15 +4,20 @@ using System.Text;
 
 
 string key = "12345678901234561234567890123456";
-string plaintexxt = "E23412345678 ha noi trái tim hồng";
+string plaintexxt = "E23412345678 ha noi mua mua";
+
+Console.WriteLine("plaintexxt: " + plaintexxt);
+Console.WriteLine("-----------------");
+
 
 for (int i = 1; i < 10; i++)
 {
-    Console.WriteLine(AESEncryptClient(plaintexxt, key));
+    string txtEncrypt = AESEncryptClient(plaintexxt, key);
+    Console.WriteLine(txtEncrypt);
+
+    Console.WriteLine("Giai ma: " +AESDecryptClient(txtEncrypt, key));
 }
 Console.ReadKey();
-
-
 
 
 string AESEncryptClient(string plainText, string secretKey)
@@ -44,4 +49,22 @@ string AESEncryptClient(string plainText, string secretKey)
         }
     }
     return Convert.ToBase64String(encrypted);
+}
+
+
+string AESDecryptClient(string encrypted, string secretKey)
+{
+    //aes 128
+    string iv = secretKey.Substring(0, 16);
+    string key = secretKey.Substring(16);
+    byte[] encryptedBytes = Convert.FromBase64String(encrypted);
+    AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
+    aes.Mode = CipherMode.CBC;
+    aes.Padding = PaddingMode.PKCS7;
+    aes.Key = Encoding.UTF8.GetBytes(key);//key
+    aes.IV = Encoding.UTF8.GetBytes(iv); //2314345645678765
+    ICryptoTransform crypto = aes.CreateDecryptor(aes.Key, aes.IV);
+    byte[] secret = crypto.TransformFinalBlock(encryptedBytes, 0, encryptedBytes.Length);
+    crypto.Dispose();
+    return Encoding.UTF8.GetString(secret);
 }
