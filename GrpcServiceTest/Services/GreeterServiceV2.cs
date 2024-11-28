52e273d6c8fcc0393a36f8f4f58214c1c6103f99
khuchuan <khuchuan@gmail.com>
2024-11-29 04:36:13 +0700

    merge

---
+++ GrpcServiceTest/Services/GreeterServiceV2.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Core;
+
+namespace GrpcServiceTest.Services;
+
+public class GreeterServiceV2 : Greeter.GreeterBase
+{
+    private readonly ILogger<GreeterServiceV2> _logger;
+    public GreeterServiceV2(ILogger<GreeterServiceV2> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = "Hello " + request.Name
+        });
+    }
+
+}
